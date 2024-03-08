68c911ab39b70682b3aa34d1664ed8a151a42e46
Paulo Firmino <paulofirmino@Paulos-MacBook-Pro.local>
2024-03-08 18:10:47 -0300

    fix build

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,5 +1,3 @@
-import { UserDataAuth } from "../contexts/ContextsFiles/UserData";
-
 export const TOKEN_KEY = "TOKEN_TRIPNESS";
 export const USER = "USER";
 
@@ -10,9 +8,3 @@ export const getToken = () => localStorage.getItem(TOKEN_KEY);
 export const login = (token: string) => localStorage.setItem(TOKEN_KEY, token);
 
 export const logout = () => localStorage.removeItem(TOKEN_KEY);
-
-export const setUser = (user: UserDataAuth) =>
-  localStorage.setItem(USER, JSON.stringify(user));
-
-export const getUser = (): UserDataAuth =>
-  JSON.parse(localStorage.getItem(USER) || "");
